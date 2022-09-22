0bc70fcdb19e5edd1ae445b7692cf6225f210803
HariramTR <114093229+HariramTR@users.noreply.github.com>
2022-09-22 16:48:35 +0530

    Add files via upload

---
+++ Sizeorrange.java
@@ -0,0 +1,13 @@
+/*3(L1). Write a program to illustrate the size or range of various data types*/
+package task1;
+public class Sizeorrange {
+		public static void main(String args[]) {
+     System.out.println("byte :  " + Byte.SIZE/8 + " bytes");
+	 System.out.println("short :  " + Short.SIZE/8 + " bytes");
+     System.out.println("int :  " + Integer.SIZE/8 + " bytes");
+	 System.out.println("long :  " + Long.SIZE/8 + " bytes");
+	 System.out.println("float :  " + Float.SIZE/8 + " bytes");
+     System.out.println("double :  " + Double.SIZE/8 + " bytes");
+	 System.out.println("char :  " + Character.SIZE/8 + " bytes");
+			}
+	}
