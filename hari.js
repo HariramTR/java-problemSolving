9be03053bd876d61e6fb4e04e24ce0657c276e37
HariramTR <114093229+HariramTR@users.noreply.github.com>
2022-10-06 19:18:02 +0530

    Add files via upload

--- hari.js
+++ hari.js
@@ -23,6 +23,7 @@ let output="";
  {name :"ram",age:20,city:"salem",salary:5000},
  {name :"babu",age:21,city:"madurai",salary:10000}];
  console.table(user);
+
 //  document.getElementById("hhh").innerHTML=user;
  let eligible = user.map((users)=>({/*name:users.name,age:users.age,city:users.city,salary:users.salary,*/
  ...users,/*ithum oru type epdium podalam - ithu easy (or) another type inside note la irukuda hari*/
@@ -60,6 +61,78 @@ console.log(d);
 const names =["Hari", "Ram", "Usha", "Nithya"];
 names.sort();
 console.log(names);
+const num1 = [7,13,5,8,14];
+num1.sort((a,b)=>{return a-b;});
+console.log("compare sort : "+num1);
+console.table(user);
+user.sort((a,b)=>{return a.age-b.age;});
+console.table(user);
+const num2 = [11,6,20,-15,19];
+num2.sort((a,b)=>{return a-b;});
+console.log("compare sort : "+num2);
+console.table(user);
+user.sort((a,b)=>{return b.age-a.age;});
+console.table(user);
+user.sort((a,b)=>{if(a.name>b.name)return 1;
+if(a.name<b.name)return -1; return 0});
+console.table(user);
+let k = [1,2,3,4,5,6,7];
+k.fill(20);
+console.log("Before fill : " +k);
+console.log(k);
+let k1 = [1,2,3,4,5,6,7];
+ k1.fill(20,3);
+console.log(k1);
+let k2 = [1,2,3,4,5,6,7];
+k2.fill(20,3,5);
+console.log(k2);
+const products = ["pen","pencil","Eraser","Box","scale","water bottle"];
+let results = products.includes("pen");
+console.log(results);
+ results = products.includes("water bottle");
+console.log(results);
+results = products.includes("pen",2);
+console.log(results);
+results = products.includes("scale",3);
+console.log(results);
+let g = [1,2,3,4,5];
+console.log(g);
+console.log(g.push(17));
+console.log (g);
+ g.push(10,20,14,13);
+console.log(g);
+let fruits = ["Apple"];
+console.log(fruits);
+fruits.push("Orange");
+console.log(fruits);
+fruits.push("Banana", "Pineapple");
+console.log(fruits);
+let users1 = ["hari", "ram","kavi","ramesh"];
+let users2 = ["ravi","mani"];
+users1.push(...users2);
+console.log(users1);
+const oops = ['ram','mani','hari','ravi'];
+console.log(oops);
+console.log(oops.pop());
+console.log(oops);
+console.log(oops.pop());  
+console.log(oops);
+console.log("****Shift in JS*****");
+let student = ["ram","ravi","hari","maran"];
+console.log("Before shift : " +student);
+const element = student.shift();
+console.log("After shift : " +student);
+console.log("Removed element : " +element);
+console.log("****Unshift in JS*****");
+let stu = ["ram","ravi","kumar","nivi"];
+console.log(stu);
+let len = stu.unshift("mani");
+console.log(stu);
+console.log(len);
+len  = stu.unshift("ajith","kumar");
+console.log(stu);
+console.log(len);
+ 
 }
 
  function Close(){
